0b04804497ee1fcc8c9d4ee9a54c6ba5d9a43001
Viktor-sun <bobick.zdoh@gmail.com>
2021-11-22 16:23:09 +0200

    feat: add todos

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("form");
+const list = document.getElementById("list");
+
+form.addEventListener("submit", handleOnForm);
+list.addEventListener("click", handleOnClick);
+
+const todos = [];
+
+function handleOnForm(e) {
+  e.preventDefault();
+
+  const todo = e.target.input.value;
+  todos.push(todo);
+  appendMarkup();
+
+  e.currentTarget.reset();
+}
+
+function handleOnClick(e) {
+  if (e.target.nodeName !== "BUTTON") return;
+
+  const index = e.target.dataset.index;
+  todos.splice(index, 1);
+  appendMarkup();
+}
+
+function appendMarkup() {
+  const markup = todos
+    .map(
+      (todo, i) =>
+        `<li class='item'>${todo} <button id="button" type="button" data-index=${i}>del</button></li>`
+    )
+    .join("");
+  list.innerHTML = markup;
+}
